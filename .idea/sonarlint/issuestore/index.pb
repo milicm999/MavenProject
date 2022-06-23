e76d36387c9e772a301a8394f7c680a227911e24
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-06-23 12:10:13 +0200

    HelloWorld Maven project
    Some dependencies were added just for example, they are not in usage, like dependency for H2...

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+T
+$Child1/src/main/java/MainChild1.java,b\e\be1e2f1e3b4bbcb4f33664035ae7b07ed54c63a5
+T
+$Child2/src/main/java/MainChild2.java,3\9\399f0764876abb1ee542b56f3e16cb0e94ec0ef3
+G
+src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
---
+++ Child2/src/test/java/TestingMainChild2.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class TestingMainChild2 {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+    @Test
+    public void testMain()
+    {
+        System.out.println("Hello Baeldung Readers!!");
+
+        Assert.assertEquals("Hello Baeldung Readers!!", outputStreamCaptor.toString()
+                .trim());
+    }
+}
+
